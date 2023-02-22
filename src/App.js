817808d5b8f644c1e1f277524c6e3313a99f9f4b
Jeff B <105983864+Jeff-Shango@users.noreply.github.com>
2023-02-22 12:58:59 -0500

    updating with navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react'
-import Products from './components/Products';
+import React, { useState, useEffect } from 'react'
+import { Commerce} from './lib/commerce';
+import { Products, Navbar } from './components';
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log(products);
+  
   return (
     <div>
+      <Navbar />
       <Products />
     </div>
   )
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
+import useStyles from './styles';
 
-import Product from './Product/Product';
+import Product from "./Product/Product";
 
 const products = [
     { id: 1, name: '1:1', description: '1:1 Personal Training.', price: '$45', image: "src/assets/personaltraining.png"},
     { id: 2, name: 'Group', description: 'Group Training.', price: '$40', image: "src/assets/personaltraining.png"},
 ]
 const Products = () => {
+    const classes = useStyles();
     return (
-    <main>
+    <main className={classes.content}>
+        <div className={classes.toolbar}/>
         <Grid container justifyContent="center" spacing={4}>
             {products.map((product) => (
                 <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
